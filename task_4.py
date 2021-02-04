c231f58f67a9d2e0a74762c9167d8244fab7de73
Ivanka2021 <78328510+Ivanka2021@users.noreply.github.com>
2021-02-04 13:45:59 +0300

    Delete task_4.py

--- task_4.py
+++
@@ -1,7 +0,0 @@
-
-my_str = input("Введите любое слово или словосочетание: ")
-a = my_str.split(' ')
-for i, el in enumerate(a, 1):
-    if len(el) > 10:
-        el = el[0:10]
-    print(f'{i} - {el}')
